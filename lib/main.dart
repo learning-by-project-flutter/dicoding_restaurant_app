e6362d9d13add9cb122ab09b25bc88838e7532cc
melanchorilla-portfolio <dickykamaludin13@gmail.com>
2025-03-04 11:13:01 +0700

    notif, belum dites

--- lib/main.dart
+++ lib/main.dart
@@ -6,14 +6,19 @@ import 'package:dicoding_restaurant_app/provider/search_provider.dart';
 import 'package:dicoding_restaurant_app/provider/main_page_provider.dart';
 import 'package:dicoding_restaurant_app/provider/theme_provider.dart';
 import 'package:dicoding_restaurant_app/screen/home/main_page.dart';
+import 'package:dicoding_restaurant_app/service/notification_service.dart';
 import 'package:dicoding_restaurant_app/style/theme/restaurant_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 
-void main() {
+void main() async {
   sqfliteFfiInit();
   databaseFactory = databaseFactoryFfi;
+
+  final notificationService = NotificationService();
+  await notificationService.init();
+
   runApp(
     MultiProvider(
       providers: [
